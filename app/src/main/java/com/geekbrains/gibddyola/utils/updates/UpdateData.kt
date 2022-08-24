c854980c2cbbd9da7e1fa71197320fef130de483
lekoal <coreglus@gmail.com>
2022-08-24 10:16:38 +0300

    Download file is success

--- app/src/main/java/com/geekbrains/gibddyola/utils/updates/UpdateData.kt
+++ app/src/main/java/com/geekbrains/gibddyola/utils/updates/UpdateData.kt
@@ -3,7 +3,7 @@ package com.geekbrains.gibddyola.utils.updates
 object UpdateData {
     private const val downloadApkUrl = "https://гибдд12.рф/img/photos/posters/app/app_new.jpg"
     private const val versionTextUrl = "https://гибдд12.рф/img/photos/posters/app/app_version.txt"
-    private const val fileName = "avarkomNew.apk"
+    private const val fileName = "/storage/emulated/0/Download/avarkomNew.apk"
 
     fun apkUrl() = downloadApkUrl
     fun versionUrl() = versionTextUrl
