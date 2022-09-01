ce7dda64be2272b44810ac7ec5af46c88184f91e
matr1x244 <zio-21@yandex.ru>
2022-09-01 20:41:53 +0300

    layout MainFragment revision

--- app/src/main/java/com/geekbrains/gibddyola/ui/main/MainFragment.kt
+++ app/src/main/java/com/geekbrains/gibddyola/ui/main/MainFragment.kt
@@ -11,7 +11,6 @@ import android.text.SpannableStringBuilder
 import android.text.Spanned
 import android.text.style.AbsoluteSizeSpan
 import android.text.style.ForegroundColorSpan
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
