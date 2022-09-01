1a9d79366f38f994af8fd227c85e74a947f7fa37
matr1x244 <zio-21@yandex.ru>
2022-09-01 07:35:17 +0300

    clear MainActivity.kt testNumber () delete

--- app/src/main/java/com/geekbrains/gibddyola/MainActivity.kt
+++ app/src/main/java/com/geekbrains/gibddyola/MainActivity.kt
@@ -1,17 +1,11 @@
 package com.geekbrains.gibddyola
 
-import android.Manifest
 import android.animation.ObjectAnimator
-import android.content.Context
-import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
-import android.telephony.TelephonyManager
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.animation.doOnEnd
-import androidx.core.app.ActivityCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.geekbrains.gibddyola.domain.employee.ControllerOpenFragment
 import com.geekbrains.gibddyola.domain.employee.EntityAvarkom
@@ -19,9 +13,6 @@ import com.geekbrains.gibddyola.ui.about.AboutFragment
 import com.geekbrains.gibddyola.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity(), ControllerOpenFragment {
-
-    private lateinit var IMEINumber: String
-    private val REQUEST_CODE = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +23,6 @@ class MainActivity : AppCompatActivity(), ControllerOpenFragment {
                 .add(R.id.main_activity_container, MainFragment.newInstance())
                 .commitNow()
         }
-        testNumber()
     }
 
     private fun startSplash() {
@@ -63,34 +53,4 @@ class MainActivity : AppCompatActivity(), ControllerOpenFragment {
             .addToBackStack("")
             .commit()
     }
-
-    fun testNumber() {
-        val telephonyManager = getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED
-        ) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_PHONE_STATE), REQUEST_CODE)
-            return
-        }
-        IMEINumber = telephonyManager.line1Number
-        println("@@@ $IMEINumber")
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<String?>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            REQUEST_CODE -> {
-                if (grantResults.isNotEmpty() && grantResults[0] ==
-                    PackageManager.PERMISSION_GRANTED
-                ) {
-                    Toast.makeText(this, "Permission granted. NUMBER", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(this, "Permission denied. NUMBER", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-    }
-
 }
