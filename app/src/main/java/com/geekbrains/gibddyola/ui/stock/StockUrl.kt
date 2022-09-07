ff7cdf4502e564752720b1682b98f4b67b9fdf1e
matr1x <privatethread.zio@gmail.com>
2022-09-07 09:18:54 +0300

    fix delay time poster and alpha = 100

--- app/src/main/java/com/geekbrains/gibddyola/ui/stock/StockUrl.kt
+++ app/src/main/java/com/geekbrains/gibddyola/ui/stock/StockUrl.kt
@@ -4,6 +4,6 @@ object StockUrl {
     const val POSTERS_ONE = "https://гибдд12.рф/img/photos/posters/1.jpg"
     const val POSTERS_TWO = "https://гибдд12.рф/img/photos/posters/2.jpg"
     const val POSTERS_THREE = "https://гибдд12.рф/img/photos/posters/3.jpg"
-    const val DELAY_TIME_POSTER = 5_00L
-    const val ALPHA_DURATION_POSTER = 500
+    const val DELAY_TIME_POSTER = 1_00L
+    const val ALPHA_DURATION_POSTER = 100
 }
