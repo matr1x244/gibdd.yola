00e1a6d1ce27345a10a48e287521199339dcf65d
andlekoal <andlekoal@gmail.com>
2022-09-15 18:54:20 +0300

    Images to string

--- app/src/main/java/com/geekbrains/gibddyola/game/domain/entity/QuestionDomain.kt
+++ app/src/main/java/com/geekbrains/gibddyola/game/domain/entity/QuestionDomain.kt
@@ -7,7 +7,7 @@ import androidx.room.PrimaryKey
 @Entity
 data class QuestionDomain(
     @PrimaryKey val id: String,
-    @ColumnInfo(name = "image") val image: Int?,
+    @ColumnInfo(name = "image") val image: String,
     @ColumnInfo(name = "question") val question: String,
     @ColumnInfo(name = "answers") val answers: List<Pair<String, Boolean>>,
     @ColumnInfo(name = "explanation") val answer_about: String
