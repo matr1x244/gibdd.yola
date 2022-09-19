76a40cb37e5766d1ed7f7a65120ad44f6e332f86
matr1x244 <zio-21@yandex.ru>
2022-09-19 23:25:32 +0300

    add onReceivedError in class MyWebViewClient

--- app/src/main/java/com/geekbrains/gibddyola/utils/vkontakte/MyWebViewClient.kt
+++ app/src/main/java/com/geekbrains/gibddyola/utils/vkontakte/MyWebViewClient.kt
@@ -1,8 +1,11 @@
 package com.geekbrains.gibddyola.utils.vkontakte
 
+import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
+import com.geekbrains.gibddyola.R
 
 class MyWebViewClient : WebViewClient() {
 
@@ -11,6 +14,17 @@ class MyWebViewClient : WebViewClient() {
         return true
     }
 
+    override fun onReceivedError(
+        view: WebView?,
+        request: WebResourceRequest?,
+        error: WebResourceError
+    ) {
+        Toast.makeText(
+            view?.context,
+            R.string.no_internet_message,
+            Toast.LENGTH_SHORT
+        ).show()
+    }
 
     override fun onPageFinished(view: WebView?, url: String?) {
         super.onPageFinished(view, url)
