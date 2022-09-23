7e0ca1c136edd9f0368ab6c02093003606ab3a8a
matr1x244 <zio-21@yandex.ru>
2022-09-23 07:28:45 +0300

    jpg to webp url

--- app/src/main/java/com/geekbrains/gibddyola/ui/stock/StockUrl.kt
+++ app/src/main/java/com/geekbrains/gibddyola/ui/stock/StockUrl.kt
@@ -1,9 +1,9 @@
 package com.geekbrains.gibddyola.ui.stock
 
 object StockUrl {
-    const val POSTERS_ONE = "https://гибдд12.рф/img/photos/posters/1.jpg"
-    const val POSTERS_TWO = "https://гибдд12.рф/img/photos/posters/2.jpg"
-    const val POSTERS_THREE = "https://гибдд12.рф/img/photos/posters/3.jpg"
+    const val POSTERS_ONE = "https://гибдд12.рф/img/photos/posters/1.webp"
+    const val POSTERS_TWO = "https://гибдд12.рф/img/photos/posters/2.webp"
+    const val POSTERS_THREE = "https://гибдд12.рф/img/photos/posters/3.webp"
     const val DELAY_TIME_POSTER = 1000L
     const val ALPHA_DURATION_POSTER = 350
 }
