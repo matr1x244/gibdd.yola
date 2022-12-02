21cdfea4863e6b67265c0beb94933837f21260a1
matr1x244 <zio-21@yandex.ru>
2022-12-03 00:06:52 +0300

    удалил проверку API vers phone для загрузки стартого экрана сделал единый экран загрузки.

--- app/src/main/java/com/geekbrains/gibddyola/StartActivityTheme.kt
+++ app/src/main/java/com/geekbrains/gibddyola/StartActivityTheme.kt
@@ -7,35 +7,48 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterInside
+import com.bumptech.glide.load.resource.bitmap.FitCenter
 
 class StartActivityTheme : AppCompatActivity() {
+
+    private lateinit var imgStart: AppCompatImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_load_theme)
 
-        val version = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+        imgStart = findViewById(R.id.iv_logo)
 
-        if (version) {
-            val screen = installSplashScreen()
-            screen.setOnExitAnimationListener { screenProvider ->
-                ObjectAnimator.ofFloat(
-                    screenProvider.view,
-                    View.ALPHA, 5f, 0f
-                ).apply {
-                    duration = 900
-                    doOnEnd {
-                        screenProvider.remove()
-                    }
-                }.start()
-            }
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        } else {
-            ObjectAnimator.ofFloat(findViewById(R.id.iv_logo), View.ALPHA, 0.2f, 1.0f)
-                .setDuration(900)
+//        val version = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+//
+//        if (version) {
+//            val screen = installSplashScreen()
+//            screen.setOnExitAnimationListener { screenProvider ->
+//                ObjectAnimator.ofFloat(
+//                    screenProvider.view,
+//                    View.ALPHA, 5f, 0f
+//                ).apply {
+//                    duration = 900
+//                    doOnEnd {
+//                        screenProvider.remove()
+//                    }
+//                }.start()
+//            }
+//            startActivity(Intent(this, MainActivity::class.java))
+//            finish()
+//        } else {
+            Glide.with(imgStart)
+                .asGif()
+                .load(R.mipmap.gif_logo_no_road)
+                .transform(FitCenter(), CenterInside())
+                .into(imgStart)
+            ObjectAnimator.ofFloat(imgStart, View.TRANSLATION_X, -800.0f, 0.0f)
+                .setDuration(700)
                 .start()
             Handler(mainLooper).postDelayed({
                 startActivity(Intent(this, MainActivity::class.java))
@@ -43,4 +56,4 @@ class StartActivityTheme : AppCompatActivity() {
             }, 900L)
         }
     }
-}
+//}
